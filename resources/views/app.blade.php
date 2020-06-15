502bfc67c2d3309d944d01e365b7a3f6445758c3
Steven Bandola <bandola.steven@gmail.com>
2020-06-15 15:42:03 -0600

    initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" />
+        <title>Skope</title>        
+    </head>
+    <body>        
+        <div id="root"></div>      
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
